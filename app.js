0b358742ed866c7d54c1ffc6ff1d0a09cc56e8ed
Ugochukwu4u <ugochukwupeter4u@gmail.com>
2025-05-28 11:45:55 +0100

    created loopstudio project

---
+++ app.js
@@ -0,0 +1,8 @@
+const nav = document.querySelector(".header-nav nav");
+document.querySelector(".icon-menu").addEventListener('click', ()=>{
+   nav.style.transform = "translateX(0)";
+});
+
+document.querySelector(".mobile-content img:last-child").addEventListener("click", ()=>{
+    nav.style.transform = "translateX(-100%)";
+})
